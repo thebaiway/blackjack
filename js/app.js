971ed644c942601201901ee021dea5712f65d4ba
Bailee <bailee.roby@gmail.com>
2022-12-12 19:05:01 -0800

    Adding card images for game and more game logic

--- js/app.js
+++ js/app.js
@@ -1,23 +1,38 @@
 let firstCard = Math.floor(Math.random() * 10) + 2
 let secondCard = Math.floor(Math.random() * 10) + 2
-let hitCard = Math.floor(Math.random() * 10) + 2
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
+function startGame() { 
+    renderGame()
+}
 
-function startGame () {
+function renderGame () {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Count: " + sum 
     if (sum <= 20) {
     message = "Hit?"
     } else if (sum === 21) {
-        message = "Blackjack! 🥳"
+        message = "Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You've busted! 😢 "
+        message = "You've busted!"
         isAlive = false
     }
 
-console.log(message)
+    messageEl.textContent = message
 
+}
+
+function hitCard () {
+    let card = Math.floor(Math.random() * 10) + 2
+    sum += card
+    cards.push(card)
+    renderGame()
 }
